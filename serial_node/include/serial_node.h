845815d581a94afa40b0d44a08a75e5263d1d87c
haigujiuajin <573705543@qq.com>
2019-11-28 19:18:48 +0800

    modify

---
+++ serial_node/include/serial_node.h
@@ -0,0 +1,19 @@
+#ifndef _SERIAL_NODE_
+#define _SERIAL_NODE_
+
+#include <string>
+#include <sstream>
+#include "ros/ros.h" 
+#include "serial/serial.h"  //ROS已经内置了的串口包 
+#include "std_msgs/String.h" 
+#include "std_msgs/Empty.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h" //amcl_pose
+#include "tf/transform_datatypes.h" //转换函数头文件
+#include "tf/tf.h"
+#include "sensor_msgs/Imu.h"
+
+#define GRAVITY 9.80665
+
+std::string imu_frame_id;
+sensor_msgs::Imu imu_msg;
+#endif
--- serial_node/src/serial_node.cpp
+++ serial_node/src/serial_node.cpp
@@ -1,17 +1,98 @@
-#include <ros/ros.h> 
-#include <serial/serial.h>  //ROS已经内置了的串口包 
-#include <std_msgs/String.h> 
-#include <std_msgs/Empty.h> 
+#include "serial_node.h"
+
+
   
 serial::Serial ser; //声明串口对象 
 unsigned char send_data[5]={0x68,0x04,0x00,0x04,0x08};
-unsigned char data;
+uint8_t data;
+int count=0;
+unsigned char buffer[100];
+unsigned char checksum=0;
+unsigned char data_length=0;
+unsigned char dev_id;
 //回调函数 
-void write_callback(const std_msgs::String::ConstPtr& msg) 
+void write_callback(const unsigned char rev_data) 
 { 
-    ROS_INFO_STREAM("Writing to serial port" <<msg->data); 
-    ser.write(msg->data);   //发送串口数据 
+    ROS_INFO_STREAM("Writing to serial port" ); 
+    ser.write(rev_data);   //发送串口数据 
 } 
+
+int Converter(const unsigned char a, const unsigned char b, const unsigned char c)
+ {
+  std::stringstream int2str, str2int;
+  int2str << (int)a % 16 << (int)b / 16 << (int)b % 16 << (int)c / 16
+          << (int)c % 16;
+  int int_result;
+  std::string str_result;
+  int2str >> str_result;
+  str2int << str_result;
+  str2int >> int_result;
+
+  if (1 == (int)a / 16) {
+    int_result = -int_result;
+  } else {
+    int_result = abs(int_result);
+  }
+
+  return int_result;
+}
+
+
+
+
+int parase(unsigned char rev_data[],int len)
+{
+    double imu_message[9];
+
+    imu_message[0] = (double)Converter(rev_data[1], rev_data[2], rev_data[3]) / 100.0;
+    imu_message[1] = (double)Converter(rev_data[4], rev_data[5], rev_data[6]) / 100.0;
+    imu_message[2] = (double)Converter(rev_data[7], rev_data[8], rev_data[9]) / 100.0;
+
+    // attention : computing acceleration should divide 1000
+    imu_message[3] = (double)Converter(rev_data[10], rev_data[11], rev_data[12]) / 100.0;
+    imu_message[4] = (double)Converter(rev_data[13], rev_data[14], rev_data[16]) / 100.0;
+    imu_message[5] = (double)Converter(rev_data[17], rev_data[18], rev_data[19]) / 100.0;
+
+    imu_message[6] = (double)Converter(rev_data[20], rev_data[21], rev_data[22]) / 100.0;
+    imu_message[7] = (double)Converter(rev_data[23], rev_data[24], rev_data[25]) / 100.0;
+    imu_message[8] = (double)Converter(rev_data[26], rev_data[27], rev_data[28]) / 100.0;
+
+    
+    imu_msg.header.frame_id = imu_frame_id;
+    imu_msg.header.stamp = ros::Time::now();
+
+    // get orientation in quaternion form
+    imu_message[0] = imu_message[0]/ 180.0 * M_PI;
+    imu_message[1] = imu_message[1]/ 180.0 * M_PI;
+    imu_message[2] = imu_message[2]/ 180.0 * M_PI;
+    //欧拉角转四元数,类型可以不同
+	geometry_msgs::Quaternion quaternion;//定义四元数
+    quaternion=tf::createQuaternionMsgFromRollPitchYaw(imu_message[0],imu_message[1],imu_message[2]);
+    
+    imu_msg.orientation.x = quaternion.x;
+    imu_msg.orientation.y = quaternion.y;
+    imu_msg.orientation.z = quaternion.z;
+    imu_msg.orientation.w = quaternion.w;
+    // imu_msg.orientation_covariance = {1e6, 0, 0, 0, 1e6, 0, 0, 0, 1e-6};
+    imu_msg.orientation_covariance = {2e-8, 0, 0, 0, 2e-8, 0, 0, 0, 1.3e-7};//协方差
+
+    // get angular velocity
+    imu_msg.angular_velocity.x = imu_message[6] / 180.0 * M_PI;
+    imu_msg.angular_velocity.y = imu_message[7] / 180.0 * M_PI;
+    imu_msg.angular_velocity.z = imu_message[8] / 180.0 * M_PI;
+    imu_msg.angular_velocity_covariance = {5e-7, 0, 0, 0, 5e-7, 0, 0, 0, 1.3e-7};
+
+    // get linear acceleration (value * g)
+    imu_msg.linear_acceleration.x = imu_message[3] * GRAVITY;
+    imu_msg.linear_acceleration.y = imu_message[4] * GRAVITY;
+    imu_msg.linear_acceleration.z = imu_message[5] * GRAVITY;
+    imu_msg.linear_acceleration_covariance = {3.511e-5, 0, 0, 0, 3.339e-5, 0, 0, 0, 3.989e-5};
+
+ 
+    return 0;
+
+     
+}
   
 int main (int argc, char** argv) 
 { 
@@ -49,7 +130,6 @@ int main (int argc, char** argv)
     { 
         return -1; 
     } 
-  
     //指定循环的频率 
     ros::Rate loop_rate(50); 
     while(ros::ok()) 
@@ -87,10 +167,13 @@ int main (int argc, char** argv)
             {
                   if(checksum==data)
                   {
-                      
+                      parase(buffer,data_length-3);
+                      read_pub.publish(imu_msg); 
                   }
+                  count=0;
+
             }
-            read_pub.publish(result); 
+            
         } 
 
         //处理ROS的信息，比如订阅消息,并调用回调函数 
