4f140cdc9362bd277133e65c3a236a96403e52fd
=haigujiujian <=573705543@qq.com>
2019-12-19 20:15:35 +0800

    add ....

---
+++ can_communication/src/can_control.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include "can_communication/controlcan.h"
+#include "can_communication/can_control.h"
+
+#define nDeviceType  4
+#define nDeviceInd  0
+#define nCANInd  0
+
+CANcommunication::CANcommunication()
+{
+
+}
+
+CANcommunication::~CANcommunication()
+{
+    
+}
+
+bool  CANcommunication::Can_Open()
+{
+	dwRel = VCI_OpenDevice(nDeviceType, nDeviceInd, 0);
+	if (dwRel != 1)
+	{
+		std::cout << "open the device failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		std::cout << "open the device success" << std::endl;
+		return TRUE;
+	}
+}
+
+bool CANcommunication::Can_Close()
+{
+	dwRel = VCI_CloseDevice(nDeviceType, nDeviceInd);
+	if (dwRel != 1)
+	{
+		std::cout << "close the device failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		std::cout << "close the device success" << std::endl;
+		return TRUE;
+	}
+}
+
+bool CANcommunication::Can_Initial()
+{
+	vic.AccCode = 0x80000008;
+	vic.AccMask = 0xFFFFFFFF;
+	vic.Filter = 1;
+	vic.Timing0 = 0x00;
+	vic.Timing1 = 0x1C;//500K baud freq
+	vic.Mode = 0;
+	dwRel = VCI_InitCAN(nDeviceType, nDeviceInd, nCANInd, &vic);
+	if (dwRel != 1)
+	{
+		VCI_CloseDevice(nDeviceType, nDeviceInd);
+		std::cout << "initial device failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		std::cout << "initial device sucess" << std::endl;
+		return TRUE;
+	}
+}
+
+bool CANcommunication::Can_Read_Device_Info()
+{
+	dwRel = VCI_ReadBoardInfo(nDeviceType, nDeviceInd, &vbi);
+	if (dwRel != 1)
+	{
+		std::cout << "get device info failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		std::cout << "get device info success" << std::endl;
+		return TRUE;
+	}
+}
+
+int CANcommunication::Can_GetReceiveNum()
+{
+	dwRel = VCI_GetReceiveNum(nDeviceType, nDeviceInd, nCANInd);
+	return dwRel;
+}
+
+bool CANcommunication::Can_ClearBuffer()
+{
+	dwRel = VCI_ClearBuffer(nDeviceType, nDeviceInd, nCANInd);
+	if (dwRel != 1)
+	{
+		std::cout << "clear buffer failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		std::cout << "clear buffer success" << std::endl;
+		return TRUE;
+	}
+}
+bool CANcommunication::Can_Start()
+{
+	if (VCI_StartCAN(nDeviceType, nDeviceInd, nCANInd) != 1)
+	{
+		VCI_CloseDevice(nDeviceType, nDeviceInd);
+		std::cout << "can start failure" << std::endl;
+		return FALSE;
+	 }
+	else
+	{
+		std::cout << "can start sucess" << std::endl;
+		return TRUE;
+	}
+}
+
+bool CANcommunication::Can_Reset()
+{
+	if (VCI_ResetCAN(nDeviceType, nDeviceInd, nCANInd)!=1)
+	{
+		std::cout << "can reset failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		std::cout << "can reset sucess" << std::endl;
+		return TRUE;
+	}
+}
+
+bool CANcommunication::Can_SendMessage(UINT SendID,UINT Data_length,BYTE SendMessage[])
+{
+	vco[0].ID = SendID;
+	vco[0].RemoteFlag = 0;
+	vco[0].ExternFlag = 0;
+	vco[0].DataLen = Data_length;
+	for (unsigned int j = 0; j < Data_length; j++)
+		vco[0].Data[j] = SendMessage[j];
+	if (VCI_Transmit(nDeviceType, nDeviceInd, nCANInd, vco, 1)==-1)
+	{
+		std::cout << "device failure" << std::endl;
+		return FALSE;
+	}
+	else if(VCI_Transmit(nDeviceType, nDeviceInd, nCANInd, vco, 1)!=1)
+	{
+		std::cout << "sendmessage failure" << std::endl;
+		return FALSE;
+	}
+	else
+	{
+		//std::cout << "sendmessage sucess" << std::endl;
+		return TRUE;
+
+	}
+
+}
+
+bool CANcommunication::Can_ReceiveMessage()
+{
+	dwRel = VCI_Receive(nDeviceType, nDeviceInd, nCANInd, vci, 2500, 0);
+	if (dwRel > 0)
+	{
+		//std::cout << "receive data sucess" << std::endl;
+		return TRUE;
+	}
+	else
+	{
+		std::cout << "the device is not exisit or usb lost,you can restart can device" << std::endl;
+		return FALSE;
+	}
+}
---
+++ can_communication/src/motor_control.cpp
@@ -0,0 +1,155 @@
+#include<stdio.h>
+#include<iostream>
+#include"can_communication/motor_control.h"
+#include"can_communication/can_control.h"
+
+  CANcommunication CAN_com1;
+  BYTE Control_Word_Stop[2]={0x06,0x00} ;
+
+Motor_Control::Motor_Control()
+{
+	CAN_com1.Can_Open();
+	CAN_com1.Can_Initial();
+	CAN_com1.Can_Start();
+	PDO_Open[0] = 0x01;
+	PDO_Open[1] = 0x00;
+	
+	
+}
+
+Motor_Control::~Motor_Control()
+{
+	this->Motor_Speed_Control(0x201, 0, 0x03,Control_Word_Stop);
+	this->Motor_Speed_Control(0x202, 0, 0x03,Control_Word_Stop);
+	this->Motor_Speed_Control(0x202, 0, 0x03,Control_Word_Stop);
+	this->Motor_Speed_Control(0x202, 0, 0x03,Control_Word_Stop);
+	CAN_com1.Can_Close();
+}
+
+
+bool Motor_Control::Motor_PDO_Open()
+{
+	
+	if (CAN_com1.Can_SendMessage(0x00, 2, PDO_Open))
+	{
+		std::cout << "PDO start  success" << std::endl;
+		return TRUE;
+	}
+	else
+	{
+		std::cout << "PDO start failure" << std::endl;
+		return FALSE;
+	}
+}
+
+void Motor_Control::Motor_Speed_Control(INT16 Motor_RPDO_ID, int Motor_Speed,BYTE WorkMode,BYTE CONTROL_Word[2])
+{
+	
+	BYTE Send_Message[7];
+	BYTE Speed_Message[4];
+	double speed;
+	speed =   (512.0*Motor_Speed*encoder_num)/1875.0 ;
+	Dec2HexVector(Speed_Message, speed, 4);
+	Send_Message[0] = CONTROL_Word[0];
+	Send_Message[1] = CONTROL_Word[1];
+	Send_Message[2] = WorkMode;
+	Send_Message[3] = Speed_Message[0];
+	Send_Message[4] = Speed_Message[1];
+	Send_Message[5] = Speed_Message[2];
+	Send_Message[6] = Speed_Message[3];
+	if (CAN_com1.Can_SendMessage(Motor_RPDO_ID, 7, Send_Message) == TRUE)
+	{
+		std::cout << "speed control send sucess" << std::endl;
+	}
+	else
+	{
+		std::cout << "speed control send failure" << std::endl;
+	}
+}
+
+
+bool Motor_Control::Motor_Feedback()
+{
+	BYTE data[2];
+
+	
+	CAN_com1.Can_SendMessage(0x80, 0, data);
+	if (CAN_com1.Can_ReceiveMessage())
+	{
+		//std::cout << "I have receive data" << std::endl;
+	
+		
+		for(int i=0;i<4;i++)
+		{
+			if(CAN_com1.vci[i].ID==0x282)
+			{
+				speed_change.real_time_speed[0]=CAN_com1.vci[i].Data[0];
+				speed_change.real_time_speed[1]=CAN_com1.vci[i].Data[1];
+                speed_change.real_time_speed[2]=CAN_com1.vci[i].Data[2];
+				speed_change.real_time_speed[3]=CAN_com1.vci[i].Data[3];
+
+				/*for(i=0;i<4;i++)
+				{
+					std::cout<<std::to_string(speed_change.real_time_speed[i])<<std::endl;
+				}*/
+				left_realtime_Speed=-((speed_change.real_speed*1875.0)/(encoder_num*512.0));				
+				std::cout<<"left_real_speed:"<<std::to_string(left_realtime_Speed)<<std::endl;
+				
+
+			}
+			else if(CAN_com1.vci[i].ID==0x281)
+			{
+                speed_change.real_time_speed[0]=CAN_com1.vci[i].Data[0];
+				speed_change.real_time_speed[1]=CAN_com1.vci[i].Data[1];
+                speed_change.real_time_speed[2]=CAN_com1.vci[i].Data[2];
+				speed_change.real_time_speed[3]=CAN_com1.vci[i].Data[3];
+				/*for(i=0;i<4;i++)
+				{
+					std::cout<<std::to_string(speed_change.real_time_speed[i])<<std::endl;
+				}*/
+				right_realtime_Speed=(speed_change.real_speed*1875.0)/(encoder_num*512.0);
+				std::cout<<"right_real_speed:"<<std::to_string(right_realtime_Speed)<<std::endl;
+				
+			}
+			
+			if(i==4)
+			{
+				return TRUE;
+			}
+		}
+
+	}
+	else
+	{
+		std::cout<<CAN_com1.vci[0].ID <<std::endl;
+		//std::cout << "I don't receive data" << std::endl;
+		return FALSE;
+	}
+
+}
+
+void Motor_Control::Dec2HexVector(BYTE *data_vec, const int &dec_value, const int &len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		data_vec[i] = (((int)dec_value >> (i * 8)) & 0xff);
+	}
+}
+
+/*int Motor_Control::ByteHex2Int(BYTE *data_vec, const int &data_vec_len)
+{
+	int result = 0;
+	int sign_judger = 0x80;
+	sign_judger = sign_judger << ((data_vec_len - 1) * 8);
+	for ( int i = 0; i < data_vec_len; i++)
+	{
+		result += (data_vec[i] << (i * 8));
+		
+	}
+	if (sign_judger&&result)
+	{
+		result = -1 * (~(result - 1));
+	}
+	return result;
+}*/
+
